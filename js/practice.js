66a1ba1f51d133d71907acc9130619f302c33949
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-02-07 15:41:10 +0200

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -344,7 +344,7 @@
 // }
 // console.log(largestNumber);
 
-// TODO SLUG================
+// TODO Task-1. SLUG================
 // function slugify(title) {
 //   const normalizedTitle = title.toLowerCase();
 //   const titleArray = normalizedTitle.split(' ');
@@ -359,3 +359,36 @@
 // console.log(slugify('English for developer')); // "english-for-developer"
 // console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
 // console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// TODO Task-2. Kомпозиція масивів=========================
+// function makeArray(firstArray, secondArray, maxLength) {
+//   let newArray = firstArray.concat(secondArray);
+
+//   if (newArray.length > maxLength) {
+//     return newArray.slice(0, maxLength);
+//     }
+//     return newArray
+// }
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+
+// TODO Task-3. Фільтрація масиву чисел===================
+// function filterArray(numbers, value) {
+//     let newArray = [];
+    
+//     for (const number of numbers) {
+//         if (number > value) {
+//             newArray.push(number);
+//         }
+//     }
+//     return newArray;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
